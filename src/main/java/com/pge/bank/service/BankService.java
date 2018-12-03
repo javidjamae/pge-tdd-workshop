6823d2e8af45e3ec5368fd7c897128e0c4e40dd1
Javid Jamae <javidjamae@gmail.com>
2018-12-03 08:54:20 -0800

    cleanup

--- src/main/java/com/pge/bank/service/BankService.java
+++ src/main/java/com/pge/bank/service/BankService.java
@@ -3,16 +3,16 @@ package com.pge.bank.service;
 public class BankService {
 
 	private AccountNumberGenerator generator;
-	
+
 	public void setAccountNumberGenerator(AccountNumberGenerator generator) {
 		this.generator = generator;
 	}
 
 	public String createAccount(String firstName, String lastName, int depositInCents, String governmentIdNumber) {
-		if ( depositInCents < 20000 ) {
+		if (depositInCents < 20000) {
 			throw new AccountCreationError("Initial deposit must be $200.00 or greater");
 		}
-		if ( firstName == "" || firstName == null) {
+		if (firstName == "" || firstName == null) {
 			throw new AccountCreationError("First name is invalid");
 		}
 		return generator.generateAccountNumber();
