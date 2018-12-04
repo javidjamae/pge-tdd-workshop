8e1b4bfffce3e3535be748e82b4fd0d93e864d2a
PGE\nxrm <nxrm@pge.com>
2018-12-03 16:54:51 -0800

    Unit Tests for withdrawal

--- src/main/java/com/tdd/bank/domain/Account.java
+++ src/main/java/com/tdd/bank/domain/Account.java
@@ -16,5 +16,17 @@ public class Account {
 		}
 		this.balanceInPennies = this.balanceInPennies+ deposit;
 	}
+
+	public void withdraw(int withdrawalAmount) throws AccountTrasactionError {
+		if (withdrawalAmount > this.balanceInPennies) {
+			throw new AccountTrasactionError("Nice try. Not enough balance to cover the withdrawal");
+		}
+		
+		if (withdrawalAmount < 0) {
+			throw new AccountTrasactionError("Nice try again. Withdrawal amount cannot be negative");
+		}
+		
+		this.balanceInPennies = this.balanceInPennies - withdrawalAmount;
+	}
 	
 }
