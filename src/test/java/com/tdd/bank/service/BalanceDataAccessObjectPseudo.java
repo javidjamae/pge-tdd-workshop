cb4ad0a2879efe24f1f56414d722c5edcf4cabb2
Javid Jamae <javidjamae@gmail.com>
2018-12-04 15:14:22 -0800

    fixed build

--- src/test/java/com/tdd/bank/service/BalanceDataAccessObjectPseudo.java
+++ src/test/java/com/tdd/bank/service/BalanceDataAccessObjectPseudo.java
@@ -9,19 +9,4 @@ public class BalanceDataAccessObjectPseudo implements BalanceDataAccessObject {
 		throw new Error("shouldn't be called");
 	}
 
-	@Override
-	public Integer getBalanceForAccount2(String accountNumber) {
-		throw new Error("shouldn't be called");
-	}
-
-	@Override
-	public Integer getBalanceForAccount3(String accountNumber) {
-		throw new Error("shouldn't be called");
-	}
-
-	@Override
-	public Integer getBalanceForAccount4(String accountNumber) {
-		throw new Error("shouldn't be called");
-	}
-
 }
