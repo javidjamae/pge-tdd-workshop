5df088470f74cc24468016955bb7897bfb2d1e51
PGE\UXN1 <uxn1@pge.com>
2018-12-04 11:46:37 -0800

    checking -in Statement Test Cases

--- src/test/java/com/tdd/bank/service/AccountStatementTest.java
+++ src/test/java/com/tdd/bank/service/AccountStatementTest.java
@@ -22,37 +22,4 @@ public class AccountStatementTest {
 		assertEquals("Jan", statement.getMonth());
 	}
 
-	/*
-	 * 
-	 * @Test public void validateGovtIdGreater10Digit() { // setup BankService
-	 * bankService = new BankService();
-	 * 
-	 * try { // execute bankService.createAccount("Javid", "Jamae", 20000,
-	 * "12344434343"); // fail("should have thrown exception"); } catch
-	 * (AccountCreationError e) { // verify assertEquals(
-	 * "Government ID cannot be longer than 10 digits", e.getMessage()); } }
-	 * 
-	 * @Test public void validateGovtIdInvalidNumber() { // setup BankService
-	 * bankService = new BankService();
-	 * 
-	 * try { // execute bankService.createAccount("Javid", "Jamae", 20000,
-	 * "12W1443433"); fail("Valid number"); // fail(
-	 * "should have thrown exception"); } catch (AccountCreationError e) { //
-	 * verify assertEquals("Government ID should be valid 10 digits number",
-	 * e.getMessage()); } }
-	 * 
-	 * @Test public void success() { // setup BankService bankService = new
-	 * BankService(); AccountNumberGenerator generator = new
-	 * AccountNumberGenerator() {
-	 * 
-	 * @Override public String generateAccountNumber() { return "987654321"; }
-	 * }; bankService.setAccountNumberGenerator(generator);
-	 * 
-	 * // execute String accountNumber = bankService.createAccount("Javid",
-	 * "Jamae", 20000, "3456789012");
-	 * 
-	 * // verify assertEquals("987654321", accountNumber);
-	 * 
-	 * }
-	 */
 }
