86f0c617d9c4df80e435c3097a794fa85c7f008f
PGE\s2dx <s2dx@pge.com>
2018-12-03 16:57:17 -0800

    cod Changes

--- src/main/java/com/tdd/bank/service/CertificateOfDepositService.java
+++ src/main/java/com/tdd/bank/service/CertificateOfDepositService.java
@@ -3,7 +3,10 @@ package com.tdd.bank.service;
 public class CertificateOfDepositService {
 
 	public int calculateDeposit(int amt, int months) {
-		System.out.println("Months: "+months);
+		
+		if(amt <0){
+			throw new CODError("Invalid Amount");
+		}
 		if(CODENUM.NINE.getValue() == months){
 			return amt * 9 * 100;
 		}else if(CODENUM.SIXTYFOUR.getValue() == months){
@@ -15,7 +18,6 @@ public class CertificateOfDepositService {
 		}else if(CODENUM.TWElVE.getValue() == months){
 			return amt * 12 * 100;
 		}else{
-			System.out.println("In Else ERROR");
 			throw new CODError("Invalid Term for Certificate of Deposit");
 		}
 	}
